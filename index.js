2890e42dea758e828f11644a1f7e4d92a2aac974
Reetta Niinisalo <reetta.niinisalo@gmail.com>
2019-12-22 18:40:23 +0200

    sovelluksen eka lataus

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 2
+  },
+  { name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+]
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+            <p>${new Date()} `)
+})
+  
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(note => note.id !== id)
+  
+  response.status(204).end()
+})
+  
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name and/or number missing' 
+    })
+  } else if (persons.map(person => person.name).includes(body.name)) {
+    return response.status(400).json({ 
+      error: 'name already in phonebook'
+    })
+  } else {
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: Math.ceil(Math.random() * 1000000)
+    }
+    
+    persons = persons.concat(person)
+    response.json(person)
+  }
+})
+  
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
